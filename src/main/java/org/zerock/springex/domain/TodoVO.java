d634d2263342171adf089b8ad2a81cd4fa1956cc
unknown <rurii@knou.com>
2025-02-17 22:07:59 +0900

    c

--- src/main/java/org/zerock/springex/domain/TodoVO.java
+++ src/main/java/org/zerock/springex/domain/TodoVO.java
@@ -1,4 +1,21 @@
 package org.zerock.springex.domain;
 
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TodoVO {
+
+    private Long tno;
+    private String title;
+    private LocalDate dueDate;
+    private String writer;
+    private boolean finished;
+
 }
